9af553eaed6d1d5cebef8564e3d9e0d3c671cbd4
Buša Radek, Ing <ja@radekbusa.eu>
2022-12-04 08:26:03 +0100

    2 - Declared reactive form and connected it to the template

--- src/app/reactive-user-advanced-info/reactive-user-advanced-info.component.ts
+++ src/app/reactive-user-advanced-info/reactive-user-advanced-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserGender } from '../model';
 
 @Component({
@@ -7,6 +8,18 @@ import { UserGender } from '../model';
   styleUrls: ['./reactive-user-advanced-info.component.css'],
 })
 export class ReactiveUserAdvancedInfoComponent implements OnInit {
+
+  form = new FormGroup({
+    firstName: new FormControl<string | undefined>(undefined, [Validators.required]),
+    lastName: new FormControl<string | undefined>(undefined, [Validators.required]),
+    gender: new FormControl<UserGender | undefined>(UserGender.MALE, [Validators.required]),
+    age: new FormControl<number | undefined>(undefined, [Validators.required, Validators.min(18)]),
+    address: new FormGroup({
+      street: new FormControl<string | undefined>(undefined, [Validators.required]),
+      city: new FormControl<string | undefined>(undefined, [Validators.required]),
+      country: new FormControl<string | undefined>(undefined, [Validators.required])
+    }),
+  });
 
   readonly UserGender = UserGender;
 
